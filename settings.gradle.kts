fe27cc002ccbe0b8f0a14fe35652e379a32f1dfb
Mohd Adnan <mohdadnan23742@gmail.com>
2024-11-05 05:21:10 +0530

    first change

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practice2(Wannabe a calc)"
+include(":app")
